6bfec592c62d604e060820484309885d967da918
Chew Yi Xin <chewyixin1999@gmail.com>
2022-09-24 15:48:38 +0800

    refactor sql script

--- backend/spm.sql
+++ backend/spm.sql
@@ -13,17 +13,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `SPM`
 --
+DROP DATABASE IF EXISTS `SPM`; 
 CREATE DATABASE IF NOT EXISTS `SPM` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `SPM`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `staff`
+-- Table structure for table `role`
 --
 
-CREATE TABLE `staff` (
-  `staff_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `role_id` int(11) PRIMARY KEY NOT NULL,
+  `role_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- INSERT INTO `role` () VALUES
+-- ()
+-- ()
+-- ();
+-- COMMIT;
+
+
+--
+-- Table structure for table `staff`
+--
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `staff_id` int(11) PRIMARY KEY NOT NULL,
   `staff_fname` varchar(50) NOT NULL,
   `staff_lname` varchar(50) NOT NULL,
   `dept` varchar(50) NOT NULL,
@@ -31,12 +49,23 @@ CREATE TABLE `staff` (
   `role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_fk_1` FOREIGN KEY (`role`) REFERENCES `role` (`role_id`);
+
+-- INSERT INTO `staff` () VALUES
+-- ()
+-- ()
+-- ();
+-- COMMIT;
+
+
 --
 -- Table structure for table `course`
 --
 
-CREATE TABLE `course` (
-  `course_id` varchar(20) NOT NULL,
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE IF NOT EXISTS `course` (
+  `course_id` varchar(20) PRIMARY KEY NOT NULL,
   `course_name` varchar(50) NOT NULL,
   `course_desc` varchar(255) DEFAULT NULL,
   `course_status` varchar(15) DEFAULT NULL,
@@ -44,74 +73,32 @@ CREATE TABLE `course` (
   `course_category` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `role`
---
+-- INSERT INTO `course` () VALUES
+-- ()
+-- ()
+-- ();
+-- COMMIT;
 
-CREATE TABLE `role` (
-  `role_id` int(11) NOT NULL,
-  `role_name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `registration`
 --
 
-CREATE TABLE `registration` (
-  `reg_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `registration`;
+CREATE TABLE IF NOT EXISTS `registration` (
+  `reg_id` int(11) PRIMARY KEY NOT NULL,
+  `staff_id` int(11) NOT NULL,
   `course_id` varchar(20) NOT NULL,
-  `staff_id` intt(11) NOT NULL,
   `reg_status` varchar(20) NOT NULL,
   `completion_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for table `staff`
---
-
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`staff_id`);
-
---
--- Indexes for table `course
---
-
-ALTER TABLE `course`
-  ADD PRIMARY KEY (`course_id`);
-
---
--- Indexes for table `role`
---
-
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`role_id`);
-
---
--- Indexes for table `registration`
---
-
-ALTER TABLE `registration`
-  ADD PRIMARY KEY (`reg_id`);
-
---
--- Constraints for table `staff`
---
-
-ALTER TABLE `staff`
-  ADD CONSTRAINT `staff_fk_1` FOREIGN KEY (`role`) REFERENCES `role` (`role_id`);
-
---
--- Constraints for table `course`
---
-
---
--- Constraints for table `role`
---
-
---
--- Constraints for table `registration`
---
-
 ALTER TABLE `registration`
   ADD CONSTRAINT `registration_fk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
   ADD CONSTRAINT `registration_fk_2` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`);
+
+-- INSERT INTO `registration` () VALUES
+-- ()
+-- ()
+-- ();
+-- COMMIT;
