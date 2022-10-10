40eb0ad1ddf3aaa1ac7587f2e76b75106709c450
Kok Wee LOH <lohkokwee@gmail.com>
2022-10-10 19:44:32 +0800

    Heroku Procfile update

--- backend/app.py
+++ backend/app.py
@@ -41,5 +41,6 @@ from services.learning_journey import get_all_learning_journeys, get_learning_jo
 
 # Run the application
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=port, debug=True)
