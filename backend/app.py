f33d1c8af6fa4d40191b8329e9e007e2f5cf57f3
Kok Wee LOH <lohkokwee@gmail.com>
2022-10-10 17:16:29 +0800

    Heroku backend test

--- backend/app.py
+++ backend/app.py
@@ -42,4 +42,5 @@ def home():
 # Run the application
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(port=port, debug=True)
