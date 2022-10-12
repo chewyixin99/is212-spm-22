e428d2fc25a411bf16cf0e06d1fb8b3ac76dd4c8
Kok Wee LOH <lohkokwee@gmail.com>
2022-10-13 04:57:31 +0800

    Heroku tests

--- backend/app.py
+++ backend/app.py
@@ -33,11 +33,11 @@ CORS(app)
 def home():
     return "<h1>G7T3 Backend</h1>"
 
-import services.staff
-import services.role
-import services.course
-import services.skill
-import services.learning_journey
+import .services.staff
+import .services.role
+import .services.course
+import .services.skill
+import .services.learning_journey
 
 # Run the application
 if __name__ == '__main__':
